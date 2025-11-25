26ef3eb37ca0fb643e9dc585771bda1f8add4852
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2025-11-25 22:33:22 +0000

    Initial Commit

---
+++ data.js
@@ -0,0 +1,32 @@
+export const articleArray  = [
+    {
+        name: "Blog one",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        date: "JULY 23, 2022"
+    },
+    {
+        name: "Blog two",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        date: "JULY 23, 2022"
+    },
+    {
+        name: "Blog three",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        date: "JULY 23, 2022"
+    },
+    {
+        name: "Blog four",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        e: "JULY 23, 2022"
+    },
+    {
+        name: "Blog five",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        date: "JULY 23, 2022"
+    },
+    {
+        name: "Blog six",
+        text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        date: "JULY 23, 2022"
+    }    
+]
---
+++ index.js
@@ -0,0 +1,3 @@
+import { articleArray } from "./data.js";
+console.log("ho");
+console.log(articleArray);
