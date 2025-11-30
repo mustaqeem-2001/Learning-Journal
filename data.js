3b4c54074038e38736642fbf570c8a37c59ecf48
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2025-11-30 21:08:54 +0000

    Added JS functionality, articles are now dynamically added, with 3 only showing on mobile view, as per the design.

--- data.js
+++ data.js
@@ -2,31 +2,37 @@ export const articleArray  = [
     {
         name: "Blog one",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        date: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-01.png"
     },
     {
         name: "Blog two",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        date: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-02.png"
     },
     {
         name: "Blog three",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        date: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-03.png"
     },
     {
         name: "Blog four",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        e: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-04.png"
     },
     {
         name: "Blog five",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        date: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-05.png"
     },
     {
         name: "Blog six",
         text: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        date: "JULY 23, 2022"
+        date: "JULY 23, 2022",
+        image: "./images/blog-image-06.png"
     }    
 ]
--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 import { articleArray } from "./data.js";
-console.log("ho");
-console.log(articleArray);
+console.log(articleArray[0].image);
+
+
+const articlesSectionEl = document.getElementById("articlesSectionEl");
+const viewMoreEl = document.getElementById("viewMoreEl");
+
+
+
+viewMoreEl.addEventListener("click", function() {
+    const htmlOutput = generateHTML();
+    viewMore(htmlOutput);
+})
+
+
+function viewMore(htmlOutput) {
+    articlesSectionEl.innerHTML = htmlOutput.join("");
+}
+
+
+// Instead of doing let htmlOutput = []; globally (which is not advised to do), 
+// I created this function to then be used as a parameter in other functions, serving as a better way.
+
+function generateHTML() {
+    const htmlOutput = articleArray.map(function(article){
+        const template = `
+            <article class="extra-articles ${article.name.replace(/ /g, "-").toLowerCase()}">
+                <img class="extra-article-image" src="${article.image}"/>
+                <p>${article.date}</p>
+                <h3>${article.name}</h3>
+                <p class="fc-505050">${article.text}</p>
+            </article>
+            `;
+        return template;
+    });
+    return htmlOutput;
+}
+
+function render(htmlOutput) {
+    articlesSectionEl.innerHTML = htmlOutput.slice(0, 3).join("");
+}
+
+const htmlOutput = generateHTML();
+render(htmlOutput);
+
